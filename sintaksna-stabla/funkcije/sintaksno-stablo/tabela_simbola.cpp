aac5468e1b01eef43cc2a1a737d81df4572c97ef
stemil01 <s.milenkovic@protonmail.com>
2025-02-26 02:32:42 +0100

    Correct typo in TabelaSimbola

--- sintaksna-stabla/funkcije/sintaksno-stablo/tabela_simbola.cpp
+++ sintaksna-stabla/funkcije/sintaksno-stablo/tabela_simbola.cpp
@@ -6,18 +6,18 @@ TabelaSimbola::~TabelaSimbola() {
     }
 }
 
-void TabelaSimbola::dodeli_vrednost(const std::string &promenjliva, Funkcija *vrednost) {
-    auto it = m_tabela.find(promenjliva);
+void TabelaSimbola::dodeli_vrednost(const std::string &promenljiva, Funkcija *vrednost) {
+    auto it = m_tabela.find(promenljiva);
     if (it != m_tabela.end()) {
         delete it->second;
     }
-    m_tabela[promenjliva] = vrednost;
+    m_tabela[promenljiva] = vrednost;
 }
 
 Funkcija *TabelaSimbola::vrednost_promenljive(const std::string &promenjliva) const {
     auto it = m_tabela.find(promenjliva);
     if (it == m_tabela.end()) {
-        std::cerr << "promenjliva " << promenjliva << " nije definisana" << std::endl;
+        std::cerr << "promenljiva " << promenjliva << " nije definisana" << std::endl;
         exit(EXIT_FAILURE);
     }
     return it->second;
